bc72ea1275478b60d093b470cc8d65a1e0ac0d99
Mathieu PATUREL <australie.p@gmail.com>
2018-12-27 11:10:31 +1100

    Add fancy animation on ghosts
    Just for fun

--- ghost.py
+++ ghost.py
@@ -8,12 +8,36 @@ class Ghost(Movable):
         super().__init__(x, y, wdx, wdy, tiles)
         self.color = pygame.Color(color) if isinstance(color, str) else color
 
+        self.frame_count = 0
+        self.angle = 0
+        # (scale, direction) the direction is added every x frame to the scale
+        self.scale = [1, 0.1]
+
+        # generate ghost image
+        self.surf = pygame.Surface((TILE_SIZE, TILE_SIZE), pygame.SRCALPHA)
+        # do the maths, it's the largest diagonal in a square. 0.707 = 1/sqrt(2)
+        side = 0.707 * TILE_SIZE
+        rect = pygame.Rect((0, 0), (side, side))
+        rect.center = self.surf.get_rect().center
+        pygame.draw.rect(self.surf, self.color, rect)
+
     def update(self):
         pass
 
     def render(self, surface):
-        center = (
-            self.ax + TILE_SIZE // 2,
-            self.ay + TILE_SIZE // 2
-        )
-        pygame.draw.circle(surface, self.color, center, TILE_SIZE // 2)
+        self.frame_count += 1
+        center = (self.ax + TILE_SIZE // 2,
+                  self.ay + TILE_SIZE // 2)
+
+        if self.frame_count % 2 == 0:
+            self.angle += 10
+            self.angle %= 360
+        if self.frame_count % 3 == 0:
+            self.scale[0] += self.scale[1]
+            if self.scale[0] > 1.2 or self.scale[0] < .8:
+                self.scale[1] = -self.scale[1]
+
+
+        surf = pygame.transform.rotozoom(self.surf, self.angle, self.scale[0])
+
+        surface.blit(surf, surf.get_rect(center=center))
